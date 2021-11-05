46358d2058300c263de1e729159794e4c5dbd149
raji <rajitha123@gmail.com>
2021-11-05 16:43:52 +0530

    2nd commit

--- src/main/java/com/mouritech/hackathon/controller/AuthController.java
+++ src/main/java/com/mouritech/hackathon/controller/AuthController.java
@@ -69,7 +69,7 @@ public class AuthController {
 }
 
 
-	@PostMapping("/signup")
+	@PostMapping("/signup1")
 	public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
 		if (userRepository.existsByUsername(signUpRequest.getUsername())) {
 			return ResponseEntity
